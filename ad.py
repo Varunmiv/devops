3401abca9b66b02e02a682270f46fd95e2bdefd9
reddy <varunklr07@gmail.com>
2024-12-26 11:37:56 +0530

    day2 c3

--- ad.py
+++ ad.py
@@ -1,6 +1,14 @@
-n=500
-i=0
-while i<=n:
- if i%3==0 and i%7==0:
-   print(i,end=" ")
- i+=1
+l=[1,2,3,[1,2],4]
+l[3][1]='varjgftyugiugun'
+l1=l.copy()
+print(id(l))
+print(id(l1))
+print(l[0],id(l[0]))
+print(l[0],id(l1[0]))
+print(l[1],id(l[1]))
+print(l[1],id(l1[1]))
+print(        )
+print(id(l[2]))
+print(id(l[3][0]))
+print(id(l[3][1]))
+print(id(l[4]))
